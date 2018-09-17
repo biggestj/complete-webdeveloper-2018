1ed6be1be06e0dba08af81751d8c00261372d7e3
Jorde Vorstenbosch <jordevorstenbosch@gmail.com>
2018-09-17 12:21:20 +0200

    changed extension to .js, finished the exercise

--- section-10/arrays/exercise6.js
+++ section-10/arrays/exercise6.js
@@ -13,9 +13,25 @@
 // 5. Sort the array in reverse order. (Not alphabetical, but reverse
 // the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
 
-you should have at the end:
-["Kiwi", "Oranges", "Blueberries"]
+// you should have at the end:
+// ["Kiwi", "Oranges", "Blueberries"]
 
 // using this array,
 // var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 // access "Oranges".
+
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(array);
+array.shift();
+console.log(array);
+array.sort();
+console.log(array);
+array.push("Kiwi");
+console.log(array);
+array.splice(0,1);
+console.log(array);
+array.reverse();
+console.log(array);
+
+let array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(array2[1][1][0]);
