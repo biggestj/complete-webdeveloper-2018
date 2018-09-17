3614ec4d4872334c7ab2e949b868874596843207
Jorde Vorstenbosch <jordevorstenbosch@gmail.com>
2018-09-17 13:30:00 +0200

    finished exercise 7

---
+++ section-10/objects-and-arrays-for-facebook-app/exercise7.js
@@ -0,0 +1,27 @@
+/* Create an object and an array which we will use in our facebook exercise. 
+    1. Create an object that has properties "username" and "password". Fill those values in with strings.
+    2. Create an array which contains the object you have made above and name the array "database".
+    3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+*/
+
+const database = [
+    {
+        username: "Jorde",
+        password: "SzechuanSauce"
+    }
+];
+
+const newsfeed = [
+    {
+        username: "Hopeful",
+        timeline: "We can timetravel"
+    },
+    {
+        username: "Hippotamus",
+        timeline: "From ancient Egypt untill now"
+    },
+    {
+        username: "Caesar",
+        timeline: "From Rome to Egypt"
+    }
+];
